4bd6facda1b8ff250db52b9510e48695e0ac310c
Aurelio <marcosaureliovm@gmail.com>
2019-11-11 10:03:21 -0300

    Enviando aplicacao para o git

---
+++ src/pages/User/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class User extends Component {
+  async componentDidMount() {}
+
+  static navigationOptions = {
+    title: 'Hello World',
+  };
+
+  render() {
+    return <View />;
+  }
+}
